128ebbacc34cce219af6a88b8d664820521c4c64
Prem Singh <hunny345686@gmail.com>
2026-06-16 14:45:54 +0530

    Fix Vite base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/path-tracker-app/'
+  plugins: [react(), tailwindcss()],
 })
